5ff2b5faa05b76d457e704c298fe3fce536c9139
Rengel Tamás <rengelt22@ady-nagyatad.sulinet.hu>
2024-09-19 09:40:24 +0200

    hfhez

---
+++ C#/Gyakorlás, programok/Házi/szorzotabla/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace szorzotabla
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Szorzótábla:");
+            Console.WriteLine();
+            
+            for (int k = 0; k <= 10; k++)
+            {
+                int ujsor = 0;
+                for (int elso = 0; elso <= 10; elso++)
+                {
+                    if (elso == 0)
+                    {
+
+                    }
+                    else
+                    {
+                        if (k == 0)
+                        {
+
+                        }
+                        else
+                        {
+                            Console.Write(k * elso + "; ");
+                            ujsor++;
+                        }
+                        
+                        
+                    }
+                    if (ujsor == 10)
+                    {
+                        Console.WriteLine();
+                    }
+
+                }
+            }
+        }
+    }
+}
