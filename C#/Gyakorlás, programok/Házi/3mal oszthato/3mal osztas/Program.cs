87055e6afd8d9a7d0189985b87abfe6bf23a8f43
RTomi05 <rengel.tamas.22@ady-nagyatad.hu>
2024-09-19 21:41:08 +0200

    miert

---
+++ C#/Gyakorlás, programok/Házi/3mal oszthato/3mal osztas/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _3mal_osztas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. feladat");
+            Console.WriteLine("3-mal osztható háromjegyű számok:");
+
+            // 1. megoldás
+            for (int i = 100; i < 1000; i++)
+            {
+                if (i % 3 == 0 && i > 100)
+                {
+                    Console.Write(i + ";");
+                }
+                
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            // 2. megoldás
+
+            for (int k = 102; k <= 999; k+=3)
+            {
+                Console.Write(k + ";");
+            }
+
+            Console.WriteLine();
+
+            //ABC
+            Console.WriteLine();
+            for (char betu = 'a'; betu <= 'z'; betu++)
+            {
+                Console.WriteLine(betu + ";");
+            }
+        }
+    }
+}
