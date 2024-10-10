301538a33604f55dcbe9857c06135095587b3285
Rengel Tamás <rengelt22@ady-nagyatad.sulinet.hu>
2024-10-10 12:47:23 +0200

    nem

---
+++ C#/Gyakorlás, programok/Tömbök/Tombproba/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Tombproba
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] szamok = new int[6];
+
+            for (int i = 0; i < szamok.Length; i++)
+            {
+                Console.Write("Írj be egy számot: ");
+                szamok[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine();
+            foreach (int i in szamok)
+            {
+                Console.Write(i + ", ");
+            }
+            
+        }
+    }
+}
