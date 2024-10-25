ab96977dbd1abea1cd849ab03175c18763d2f17c
Rengel Tamás <rengelt22@ady-nagyatad.sulinet.hu>
2024-10-25 08:45:48 +0200

    gyakorlas2.0

---
+++ C#/Gyakorlás, programok/Gyakorlás/gyakorlas1.0/Program.cs
@@ -0,0 +1,96 @@
+﻿namespace gyakorlas1_0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Változatok
+            //elso();
+            masodik();
+            
+        }
+
+        static void elso()
+        {
+            Console.WriteLine("Számkitalálós játék 1.0 Version");
+            Console.WriteLine();
+
+            Random rand = new Random();
+            int szam = rand.Next(1, 30);
+
+            int lepesek = 0;
+
+            int beirtSzam = 0;
+
+            Console.WriteLine("Melyik számra gondoltam?");
+            while (beirtSzam != szam)
+            {
+                beirtSzam = Convert.ToInt32(Console.ReadLine());
+                lepesek++;
+
+                if (beirtSzam < szam)
+                {
+                    Console.WriteLine("A szám nagyobb, mint a beírt.");
+                }
+
+                else if (beirtSzam > szam)
+                {
+                    Console.WriteLine("A szám kisebb, mint a beírt.");
+                }
+
+                else if (beirtSzam == szam)
+                {
+                    Console.WriteLine("Eltaláltad" + " " + lepesek + " " + "lépésből.");
+
+                }
+            }
+
+            Console.WriteLine();
+        }
+
+        static void masodik()
+        {
+            int szamom;
+            int pcszam = 0;
+
+            //Random
+            Random rand = new Random();
+            
+
+            Console.Write("Számom: ");
+            szamom = Convert.ToInt32(Console.ReadLine());
+
+            while (szamom != pcszam)
+            {
+
+                pcszam = rand.Next(1, 30);
+
+                if (szamom < pcszam)
+                {
+                    Console.WriteLine("A szám kisebb, mint a pc-s");
+                }
+
+                else if (szamom > pcszam)
+                {
+                    Console.WriteLine("A szám nagyobb, mint a pc-s");
+                }
+
+                else if (szamom == pcszam)
+                {
+                    Console.WriteLine("Eltaláltad.");
+                }
+            }
+        }
+
+
+        // Számkitalálós játék
+
+        //1. változat
+        //A pc gondol egy számra előre beállított keretek között, majd ki kell találni a számot!
+        //A gép ad segítséget, hogy kisebb vagy nagyobb számra gondolt!
+        //Hány lépésből sikerült kitalálni?
+
+        //2. változat
+        // A felhasználó gondol egy számra és a gépnek kell kitalálnia!
+    }
+}
---
+++ C#/Gyakorlás, programok/Gyakorlás/gyakorlas2.0/gyakorlas2/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace gyakorlas2
+{
+	internal class Program
+	{
+		static Random rand = new Random();
+		static int Generalas()
+		{
+			int szam = rand.Next(100, 1000);
+			return szam;
+		}
+
+		/*
+		 012 058 098 007
+		 100 189 157
+		 254 221 223
+		 */
+
+		static int[,] matrix()
+		{
+			int[,] vissza = new int[10, 10];
+			for (int i = 0; i < vissza.GetLength(0); i++)
+			{
+				for (int k = 0; k < vissza.GetLength(1); k++)
+				{
+					vissza[i, k] = rand.Next();
+				}
+			}
+
+			return vissza;
+		}
+
+
+
+		static void Main(string[] args)
+		{
+			//Háromjegyű véletlenszám generálás
+
+			Random rand = new Random();
+			int szam = rand.Next(100, 1000);
+			Console.WriteLine(szam);
+
+
+			//Függvénnyel
+
+			int[] szamok = new int[100];
+
+			for (int i = 0; i < 100; i++)
+			{
+				szamok[i] = Generalas();
+			}
+
+			for (int i = 0;i < szamok.Length;i++)
+			{
+				Console.WriteLine(szamok[i]);
+			}
+			
+		}
+	}
+}
