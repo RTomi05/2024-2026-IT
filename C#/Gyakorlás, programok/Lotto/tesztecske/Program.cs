dace2d4e34d3e9184efbe46a09b4ed6de66a1f99
Rengel Tamás <rengelt22@ady-nagyatad.sulinet.hu>
2024-11-11 10:01:38 +0100

    hehh

---
+++ C#/Gyakorlás, programok/Lotto/tesztecske/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+class LottoSorsolo
+{
+    static void Main()
+    {
+        // Lottó számok generálása
+        List<int> lottoszamok = GenerálLottóSzámokat(6, 1, 90);
+
+        // Kiírás
+        Console.WriteLine("A húzott lottószámok:");
+        foreach (int szam in lottoszamok)
+        {
+            Console.WriteLine(szam);
+        }
+    }
+
+    // Metódus, ami véletlenszerű lottószámokat generál, ismétlés nélkül
+    static List<int> GenerálLottóSzámokat(int darab, int min, int max)
+    {
+        Random random = new Random();
+
+        // Az összes lehetséges számot betesszük egy listába
+        List<int> lehetségesSzámok = new List<int>();
+        for (int i = min; i <= max; i++)
+        {
+            lehetségesSzámok.Add(i);
+        }
+
+        List<int> kiválasztottSzámok = new List<int>();
+
+        // Kiválasztjuk a szükséges számokat
+        for (int i = 0; i < darab; i++)
+        {
+            // Véletlenszerű szám kiválasztása a listából
+            int index = random.Next(lehetségesSzámok.Count);
+            int kiválasztottSzám = lehetségesSzámok[index];
+
+            // A kiválasztott számot eltávolítjuk a listából, hogy ne választhassuk újra
+            lehetségesSzámok.RemoveAt(index);
+
+            // A kiválasztott számot hozzáadjuk az eredményhez
+            kiválasztottSzámok.Add(kiválasztottSzám);
+        }
+
+        // A számokat rendezzük növekvő sorrendbe
+        kiválasztottSzámok.Sort();
+        return kiválasztottSzámok;
+    }
+}
