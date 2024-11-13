56fe31bd4d491d72f5c344b77a8e5ef6af10387b
Rengel Tamás <rengelt22@ady-nagyatad.sulinet.hu>
2024-11-13 14:19:03 +0100

    go

---
+++ C#/Gyakorlás, programok/Gyakorlás/gyakorlas3.0/Ciklusoktarhaza/Program.cs
@@ -0,0 +1,192 @@
+﻿namespace Ciklusoktarhaza
+{
+    internal class Program
+    {
+
+        static Random rand = new Random();
+        static void Main(string[] args)
+        {
+            elsoFuggveny();
+            masodikFuggveny();
+            harmadikFuggveny();
+            negyedikFuggveny(rand.Next(20));
+            otodikFuggveny("hehe");
+            hatodikFuggveny(rand.Next(60));
+            hetedikFuggveny("hehe", "haha");
+            nyolcadikFuggveny();
+            kilencedikFuggveny();
+            tizedikFuggveny();
+            Console.WriteLine();
+
+            Console.WriteLine();
+
+            for (int i = 0; i < 1; i++)
+            {
+                Console.WriteLine("Első for ciklus.");
+                Console.WriteLine();
+            }
+
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("Második for ciklus.");
+                Console.WriteLine();
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Harmadik for ciklus.");
+                Console.WriteLine();
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Negyedik for ciklus.");
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Ötödik for ciklus.");
+            }
+
+            Console.WriteLine();
+
+            string[] tomb = ["Igen", "Nem"];
+
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                Console.WriteLine(tomb[i]);
+            }
+
+            Console.WriteLine();
+
+            int[] tombSzamok = [1,2,3,4,5];
+
+            foreach (int i in tombSzamok)
+            {
+                if (i < tombSzamok.Length)
+                {
+                    Console.Write(i + ", ");
+                }
+
+                else
+                {
+                    Console.Write(i);
+                }            
+            }
+
+            Console.WriteLine();
+
+            int elsoValtozo = 0;
+
+            while (elsoValtozo != 5)
+            {
+
+                for (int i = 0; i < 5; i++)
+                {
+                    elsoValtozo++;
+                }
+
+                if (elsoValtozo == 5)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Első változós while jó!");
+                }
+            }
+
+            Console.WriteLine();
+
+            string elsoString = "alma";
+            int gyumolcs = 0;
+
+            while (elsoString != "banán")
+            {
+                gyumolcs++;
+
+                if (gyumolcs == 5)
+                {
+                    elsoString = "banán";
+                    Console.WriteLine("Ha kiírja a banánt, akkor jó!");
+                    Console.WriteLine(elsoString);
+                }
+            }
+
+            while (gyumolcs != 10)
+            {
+                Console.WriteLine("Még nem elég a szám." + "(" + gyumolcs + ")");
+                gyumolcs++;
+
+                if (gyumolcs == 10)
+                {
+                    Console.WriteLine("Elég a szám.");
+                }
+            
+            }
+
+            Console.WriteLine("Elég volt");
+            
+        }
+
+        static void elsoFuggveny()
+        {
+            Console.WriteLine("Első függvény");
+        }
+
+        static void masodikFuggveny()
+        {
+            Console.WriteLine("Második függvény");
+        }
+        static void harmadikFuggveny()
+        {
+            Console.WriteLine("Harmadik függvény");
+        }
+        static int negyedikFuggveny(int szam1)
+        {
+            return szam1;
+        }
+
+        static string otodikFuggveny(string szoveg1)
+        {
+            return szoveg1;
+        }
+        static int hatodikFuggveny(int szam2)
+        {
+            return szam2;
+        }
+
+        static string hetedikFuggveny(string szoveg2, string szoveg21)
+        {
+            return szoveg2 + " " + szoveg21;
+        }
+
+        static void nyolcadikFuggveny()
+        {
+
+            Console.WriteLine("Ez a nyolcadik függvény");
+        }
+
+        static void kilencedikFuggveny()
+        {
+            Console.WriteLine("Ez a kilencedik függvény.");
+        }
+
+        static void tizedikFuggveny()
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                if (i < 10)
+                {
+                    Console.Write(i + ", ");
+                }
+
+                else
+                {
+                    Console.Write(i);
+                }
+                
+            }
+         
+        }
+    }
+}
---
+++ C#/Gyakorlás, programok/doga/DatumGenerator/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Threading.Tasks.Dataflow;
+
+namespace DatumGenerator
+{
+    internal class Program
+    {
+        static Random rand = new Random();
+        static void Main(string[] args)
+        {
+
+            string[] honapok = { "január", "február", "március", "április", "május", "június", "július", "augusztus", "szeptember", "október", "november", "december"};
+            int[] napokSzama = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+            //datumCsinalo(/*ide kell két paraméter*/);
+            EvSzam();
+
+            int jo = 0;
+
+            while (jo != 1)
+                {
+                Console.Write("Adjon meg egy számot 10 és 100 között: ");
+                int randomka = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+                if (randomka >= 10 && randomka <= 100)
+                {
+                    jo++;
+                    Console.WriteLine("A számod " + randomka + ", megfelelő.");
+                }
+                else
+                {
+                    Console.WriteLine("A számod " + randomka + ", nem megfelelő.");
+                }
+                
+            }
+        }
+        
+        static string datumCsinalo(int[] ho, int[] nap)
+        {
+
+            int kerdeses = 0;
+            int hoSzam = rand.Next(1, 13);
+
+            for (int i = 0; i < nap.Length; i++)
+            {
+                kerdeses = nap[i];
+            }
+
+            int kapottNap = rand.Next(0, kerdeses);
+            string Ho = Convert.ToString(ho[hoSzam]);
+            //Ez miért nem jó :D
+
+            Console.WriteLine(kerdeses);
+
+
+
+        }
+        static void EvSzam()
+        {
+            int ezazev = rand.Next(1924, 2024);
+            Console.WriteLine(ezazev);
+
+            string[] megfeleloadatszerkezet = {};
+            string ujDatum = ezazev + Ho;
+
+            megfeleloadatszerkezet[0] = ujDatum;
+            Console.WriteLine(megfeleloadatszerkezet);
+            
+        }
+        
+
+               
+    }
+}
