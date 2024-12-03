ce6c0b00305f6b90b6538e94d0cab3ed47e0b9cc
Rengel Tamás <rengelt22@ady-nagyatad.sulinet.hu>
2024-12-03 08:45:47 +0100

    szornyu

---
+++ C#/Gyakorlás, programok/Gyakorlás/gyakorlás dictionary/dictionary_gyakorlas/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace dictionary_gyakorlas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("--------------Dictionary--------------");
+            //Példányosítás
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+            dict.Add("Ádám", "Fortnite izé");
+            dict.Add("Zoli", "Wrum-wrum");
+            dict.Add("Balázs", "PHP SQL király");
+
+            Console.WriteLine(dict["Ádám"]);
+            Console.WriteLine(dict["ádám"]);
+
+        }
+    }
+}
