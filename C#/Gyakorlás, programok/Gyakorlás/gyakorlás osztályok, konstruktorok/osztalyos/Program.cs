06231738b990ba12376a94dbce0132a23f4b5225
Rengel Tamás <rengelt22@ady-nagyatad.sulinet.hu>
2024-12-06 13:31:05 +0100

    oop

---
+++ C#/Gyakorlás, programok/Gyakorlás/gyakorlás osztályok, konstruktorok/osztalyos/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace osztalyos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            macska m = new macska("Cirmi");
+            Console.WriteLine(m.nev);
+        }
+    }
+}
