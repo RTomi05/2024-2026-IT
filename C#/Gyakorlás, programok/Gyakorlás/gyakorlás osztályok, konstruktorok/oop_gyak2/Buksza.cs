faea7d34b5b188526a2ad7e047dad3a61ff662d8
Rengel Tamás <rengelt22@ady-nagyatad.sulinet.hu>
2024-12-11 14:19:42 +0100

    menjunk :D

---
+++ C#/Gyakorlás, programok/Gyakorlás/gyakorlás osztályok, konstruktorok/oop_gyak2/Buksza.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace oop_gyak2
+{
+    internal class Buksza
+    {
+        int penz;
+
+        public string szin;
+        
+        public int Penz
+        {
+            get
+            {
+                return penz;
+            }
+            set
+            {
+                penz = value;
+            }
+        }
+
+
+
+        public Buksza(int penz)
+        {
+            Penz = penz;
+        }
+        public Buksza(int penz, string szin)
+        {
+            Penz = penz;
+            this.szin = szin;
+
+            
+        }
+
+        public override string ToString()
+        {
+            return $"{szin} buksza, benne {penz} forint.";
+        }
+
+    }
+}
---
+++ C#/Gyakorlás, programok/Gyakorlás/gyakorlás osztályok, konstruktorok/oop_gyak2/Program.cs
@@ -0,0 +1,12 @@
+﻿using oop_gyak2;
+
+Buksza buk = new Buksza(1000);
+
+Console.WriteLine(buk.Penz);
+buk.szin = "black";
+Console.WriteLine(buk.szin);
+
+Buksza buk2 = new Buksza(20000, "red");
+Console.WriteLine(buk2.szin);
+
+Console.WriteLine(buk2);
