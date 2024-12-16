2bf5c2b359cacb274f0cfc8231b0ad488c0a402c
Rengel Tamás <rengelt22@ady-nagyatad.sulinet.hu>
2024-12-16 13:30:15 +0100

    gyakolras

---
+++ C#/Gyakorlás, programok/Gyakorlás/expedicio/Expedicio.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace expedicio
+{
+    internal class Expedicio
+    {
+        public int napSorszam;
+        public int radioAmator;
+        public string feljegyzes;
+        public int felnottFarkas =- 1;
+        public int kolyokFarkas =- 1;
+
+        public Expedicio(string sor1, string sor2)
+        {
+            string[] vag = sor1.Split(" ");
+            napSorszam = int.Parse(vag[0]);
+            radioAmator = int.Parse(vag[1]);
+            feljegyzes = sor2;
+        }
+
+        public void farkasKeres()
+        {
+            string[] vag = feljegyzes.Split(" ") [0].Split("/");
+            try
+            {
+                felnottFarkas = Convert.ToInt32(vag[0]);
+                kolyokFarkas = Convert.ToInt32(vag[1]);
+            }
+            catch (Exception)
+            {
+                //dob egy hibaüzenetet
+                //throw; -> megállítja a programot
+            }
+            
+        }
+    }
+}
---
+++ C#/Gyakorlás, programok/Gyakorlás/futar/fuvar.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace futar
+{
+    internal class Fuvar
+    {
+        public int nap;
+        public int fuvarSzam;
+        public int km;
+
+        public Fuvar(string sor)
+        {
+            string[] vag = sor.Split(" ");
+
+            nap = Convert.ToInt32(vag[0]);
+            fuvarSzam = Convert.ToInt32(vag[1]);
+            km = Convert.ToInt32(vag[2]);
+        }
+    }
+}
