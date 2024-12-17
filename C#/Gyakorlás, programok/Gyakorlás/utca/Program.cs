030c5bcd8cd0d070165ddb32b14ca4fc561c8df9
Rengel Tamás <rengelt22@ady-nagyatad.sulinet.hu>
2024-12-17 08:46:19 +0100

    gyakolras

---
+++ C#/Gyakorlás, programok/Gyakorlás/utca/Program.cs
@@ -0,0 +1,28 @@
+﻿using utca;
+
+string[] sorok = File.ReadAllLines("kerites.txt");
+
+List<Telek> telkek = new List<Telek>();
+
+int parosDb = 0;
+
+
+for (int i = 0; i < sorok.Length; i++)
+{
+    telkek.Add(new Telek(sorok[i]));
+    if (telkek[telkek.Count - 1].paros)
+    {
+        parosDb++;
+        telkek[telkek.Count - 1].hazSzam = parosDb * 2;
+    }
+    else
+    {
+        telkek[telkek.Count - 1].hazSzam = (telkek.Count - parosDb)*2-1;
+    }
+        
+}
+
+Console.WriteLine("2. feladat\nAz eladott telkek száma: {0}",telkek.Count);
+
+Console.WriteLine("3. feladat\nA páros oldalon adták el ");
+Console.WriteLine(telkek[telkek.Count - 1].paros?"Páros:")
---
+++ C#/Gyakorlás, programok/Gyakorlás/utca/Telek.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace utca
+{
+    internal class Telek
+    {
+
+        public bool paros;
+        public int szelesseg;
+        public string szin;
+        public int hazSzam;
+
+        public Telek(string sor)
+        {
+            string[] vag = sor.Split(" ");
+            paros = vag[0] == "0";
+            szelesseg = Convert.ToInt32(vag[1]);
+            szin = vag[2];
+        }
+    }
+}
