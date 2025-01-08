d0554291902a791aaac6948a667e47e1635554ae
Rengel Tamás <rengelt22@ady-nagyatad.sulinet.hu>
2025-01-08 14:20:25 +0100

    orai

---
+++ C#/Gyakorlás, programok/Gyakorlás/Autók Mozgása/autok/Feladat.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace autok
+{
+    internal class Feladat
+    {
+        List<Adatok> autok = new List<Adatok>();
+
+        public Feladat()
+        {
+            feladat1();
+            feladat2();
+            feladat3();
+        }
+        void feladat1()
+        {
+            string[] sorok = File.ReadAllLines("jeladas.txt");
+
+            for (int i = 0; i < sorok.Length; i++)
+            {
+                autok.Add(new Adatok(sorok[i]));
+            }
+        }
+
+        public void feladat2()
+        {
+            Console.WriteLine("2.feladat:");
+            Console.WriteLine("Az utolsó jeladás időpontja {0}:{1}, a jármű rendszáma {2}",
+                autok[autok.Count-1].ora,
+                autok[autok.Count - 1].perc,
+                autok[autok.Count - 1].rendszam);
+        }
+
+        void feladat3()
+        {
+
+        }
+    }
+}
--- C#/Gyakorlás, programok/Gyakorlás/Autók Mozgása/autok/Program.cs
+++ C#/Gyakorlás, programok/Gyakorlás/Autók Mozgása/autok/Program.cs
@@ -1,7 +1,9 @@
-﻿internal class Program
+﻿using autok;
+
+internal class Program
 {
-    private static void Main(string[] args)
+    static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Feladat f = new Feladat();
     }
 }
