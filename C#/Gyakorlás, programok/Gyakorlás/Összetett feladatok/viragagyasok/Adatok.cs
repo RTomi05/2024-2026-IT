4468a6f590b500b4e9addf03b788c649b9c2041c
RTomi05 <rengel.tamas.22@ady-nagyatad.hu>
2025-01-31 13:30:32 +0100

    aha

---
+++ C#/Gyakorlás, programok/Gyakorlás/Összetett feladatok/viragagyasok/Adatok.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace park
+{
+    internal class Adatok
+    {
+        public int kezd;
+        public int veg;
+        public string szin;
+        public int sorszam;
+
+        public Adatok(string sor, int sorszam)
+        {
+            string[] vag = sor.Split(" ");
+
+            kezd = int.Parse(vag[0]);
+            veg = int.Parse(vag[1]);
+            szin = vag[2];
+            this.sorszam = sorszam;
+        }
+
+        public bool kapuKetOldal
+        {
+            get
+            {
+                return kezd > veg;  
+            }
+        }
+
+        public bool benneVanEz(int sorszam)
+        {
+            return (sorszam >= kezd && sorszam <= veg)
+                || (kapuKetOldal && (kezd <= sorszam || veg >= sorszam));
+        }
+    }
+}
---
+++ C#/Gyakorlás, programok/Gyakorlás/Összetett feladatok/viragagyasok/Program.cs
@@ -0,0 +1,45 @@
+﻿using park;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string[] sorok = File.ReadAllLines("felajanlas.txt");
+
+        List<Adatok> felajanlasok = new List<Adatok>();
+
+        int agyasDb = int.Parse(sorok[0]);
+
+        for (int i = 1; i < sorok.Length; i++)
+        {
+            felajanlasok.Add(new Adatok(sorok[i],i));
+        }
+
+        Console.WriteLine();
+        Console.WriteLine($"2. feladat:\nA felajánlások száma: {felajanlasok.Count}");
+
+        Console.WriteLine();
+
+        //A bejárat mindkét oldalán ültetők: 10 34 98 107 115 142 156 160 340 360 378
+        Console.WriteLine("3. feladat:");
+        Console.Write("A bejárat mindkét oldalán ültetők: ");
+
+        for(int i = 0; i < felajanlasok.Count; i++)
+        {
+            if (felajanlasok[i].kapuKetOldal)
+            {
+                Console.Write(felajanlasok[i].sorszam + " ");
+            }
+        }
+        Console.WriteLine();
+        Console.WriteLine();
+
+        //4. feladat
+        Console.Write("4. feladat:\nAdja meg az ágyás sorszámát! ");
+
+        int agyas = int.Parse(Console.ReadLine());
+
+
+
+    }
+}
