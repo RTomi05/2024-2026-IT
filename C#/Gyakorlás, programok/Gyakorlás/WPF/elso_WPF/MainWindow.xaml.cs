22c54c4128f8cd7979ac5996b6f3d75fdea4cb25
RTomi05 <rengel.tamas.22@ady-nagyatad.hu>
2025-02-19 14:20:35 +0100

    wpf

---
+++ C#/Gyakorlás, programok/Gyakorlás/WPF/elso_WPF/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace elso_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            listBox.ItemsSource = adatok;
+        }
+
+        List<string> adatok = new List<string>();
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            /*
+            string csere = nev.Text;
+            nev.Text = jelszo.Text;
+            jelszo.Text = csere;
+            */
+
+            adatok.Add(nev.Text + ":" + jelszo.Text);
+        }
+        /*
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+        */
+
+
+    }
+}
