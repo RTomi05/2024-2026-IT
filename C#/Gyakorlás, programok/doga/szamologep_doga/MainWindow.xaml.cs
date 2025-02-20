f358b24287308d18ade055fec844a9dfd9107b50
RTomi05 <rengel.tamas.22@ady-nagyatad.hu>
2025-02-20 13:45:42 +0100

    doga

---
+++ C#/Gyakorlás, programok/doga/szamologep_doga/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace szamologep
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int kapottOsszeg = 0;
+            int a = Convert.ToInt32(aSzam.Text);
+            int b = Convert.ToInt32(bSzam.Text);
+
+            kapottOsszeg = a + b;
+
+            osszeg.Content = kapottOsszeg;
+        }
+    }
+}
