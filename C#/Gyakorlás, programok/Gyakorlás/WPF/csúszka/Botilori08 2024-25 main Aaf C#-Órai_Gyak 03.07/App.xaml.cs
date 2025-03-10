079f403b92e2dbef5215ded950e20f1c6835f1b0
RTomi05 <rengel.tamas.22@ady-nagyatad.hu>
2025-03-10 13:15:30 +0100

    heh

---
+++ C#/Gyakorlás, programok/Gyakorlás/WPF/csúszka/Botilori08 2024-25 main Aaf C#-Órai_Gyak 03.07/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Gyak_03._07
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ C#/Gyakorlás, programok/Gyakorlás/WPF/csúszka/Botilori08 2024-25 main Aaf C#-Órai_Gyak 03.07/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Gyak_03._07
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            List<string> list = new List<string>();
+            list.Add("Pá rizs!");
+            list.Add("London-don-don-don");
+            list.Add("AmstelDamn");
+            list.Add("Kappanhágó");
+
+            comboBox.ItemsSource = list;
+        }
+
+
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            szamHelye.Text = csuszka.Value.ToString();
+        }
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                csuszka.Value = Convert.ToDouble(szamHelye.Text);
+            }
+            catch { 
+            
+            }
+        }
+    }
+}
