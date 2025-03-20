6ee90faa01a6b5339a4eb987ee0a26198473bd54
RTomi05 <rengel.tamas.22@ady-nagyatad.hu>
2025-03-20 14:04:43 +0100

    hehe

--- C#/Gyakorlás, programok/Gyakorlás/WPF/WPF_canvas/MainWindow.xaml.cs
+++ C#/Gyakorlás, programok/Gyakorlás/WPF/WPF_canvas/MainWindow.xaml.cs
@@ -43,7 +43,7 @@ namespace WPF_canvas
             
             timer.Interval = TimeSpan.FromMilliseconds(40);
             timer.Tick += korRajzol;
-            timer.Start();
+            //timer.Start();
             /*
             for (int i = 0; i < 36; i++)
             {
@@ -51,6 +51,52 @@ namespace WPF_canvas
                 canvas.Children.Clear();
             }
             */
+
+            szamlap(0, 0, 100);
+        }
+        void szamlap(int x, int y, int r)
+        {
+            Ellipse ellipse = new Ellipse();
+            ellipse.Width = 2 * r;
+            ellipse.Height = 2 * r;
+            ellipse.Stroke = Brushes.Black;
+            ellipse.StrokeThickness = 5;
+            ellipse.Margin = new Thickness(x, y, 0, 0);
+            canvas.Children.Add(ellipse);
+
+            for (int i = 0; i < 12; i++)
+            {
+                Line vonal = new Line();
+                vonal.Stroke = Brushes.Black;
+                vonal.X1 = x + r;
+                vonal.Y1 = y + r;
+
+                double dY = r * Math.Sin(i * 30 / 180.0 * Math.PI);
+                double dX = r * Math.Cos(i * 30 / 180.0 * Math.PI);
+
+                /*
+                double dY1 = (r - 10) * Math.Sin(i * 30 / 180.0 * Math.PI);
+                double dX1 = (r - 10) * Math.Cos(i * 30 / 180.0 * Math.PI);
+                double dY2 = (r + 10) * Math.Sin(i * 30 / 180.0 * Math.PI);
+                double dX2 = (r + 10) * Math.Cos(i * 30 / 180.0 * Math.PI);
+                */
+
+                double dY1 = 7 / 8.0 * r * Math.Sin(i * 30 / 180.0 * Math.PI);
+                double dX1 = 7 / 8.0 * r * Math.Cos(i * 30 / 180.0 * Math.PI);
+                double dY2 = 9 / 8.0 * r * Math.Sin(i * 30 / 180.0 * Math.PI);
+                double dX2 = 9 / 8.0 * r * Math.Cos(i * 30 / 180.0 * Math.PI);
+
+                vonal.X2 = x + r + dX;
+                vonal.Y2 = y + r + dY;
+
+                vonal.X1 = x + r + dX1;
+                vonal.Y1 = y + r + dY1;
+                vonal.X2 = x + r + dX2;
+                vonal.Y2 = y + r + dY2;
+
+
+                canvas.Children.Add(vonal);
+            }
         }
 
         int szogAllas = 0;
