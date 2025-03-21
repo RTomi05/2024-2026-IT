a41abda8f2a8a44a693adcb981789ce884c49856
RTomi05 <rengel.tamas.22@ady-nagyatad.hu>
2025-03-21 08:48:02 +0100

    no

--- C#/Gyakorlás, programok/Gyakorlás/WPF/WPF_canvas/MainWindow.xaml.cs
+++ C#/Gyakorlás, programok/Gyakorlás/WPF/WPF_canvas/MainWindow.xaml.cs
@@ -40,7 +40,7 @@ namespace WPF_canvas
             canvas.Children.Add(myLine);
             */
 
-            
+
             timer.Interval = TimeSpan.FromMilliseconds(40);
             timer.Tick += korRajzol;
             //timer.Start();
@@ -53,7 +53,47 @@ namespace WPF_canvas
             */
 
             szamlap(0, 0, 100);
+            mutato(100, 100, "kismutató", 0, 50);
+            mutato(100, 100, "nagymutató", 30, 80);
         }
+
+        void mutato(int x, int y, string fajta, int szog, int hossz)
+        {
+            Line vonal = new Line();
+
+            switch (fajta)
+            {
+                case "kismutató":
+                    vonal.Stroke = Brushes.Blue;
+                    vonal.StrokeThickness = 5;
+                    break;
+                case "nagymutató":
+                    vonal.Stroke = Brushes.Green;
+                    vonal.StrokeThickness = 2;
+                    break;
+                case "másodpercmutató":
+                    vonal.Stroke = Brushes.Black;
+                    vonal.StrokeThickness = 1;
+                    break;
+                default:
+                    vonal.Stroke = Brushes.Red;
+                    vonal.StrokeThickness = 1;
+                    break;
+            }
+
+            double dY = hossz * Math.Sin(szog / 180.0 * Math.PI);
+            double dX = hossz * Math.Cos(szog / 180.0 * Math.PI);
+
+
+            vonal.X1 = x;
+            vonal.Y1 = y;
+            vonal.X2 = x + dX;
+            vonal.Y2 = y + dY;
+
+            canvas.Children.Add(vonal);
+        }
+
+
         void szamlap(int x, int y, int r)
         {
             Ellipse ellipse = new Ellipse();
@@ -68,8 +108,6 @@ namespace WPF_canvas
             {
                 Line vonal = new Line();
                 vonal.Stroke = Brushes.Black;
-                vonal.X1 = x + r;
-                vonal.Y1 = y + r;
 
                 double dY = r * Math.Sin(i * 30 / 180.0 * Math.PI);
                 double dX = r * Math.Cos(i * 30 / 180.0 * Math.PI);
@@ -81,16 +119,20 @@ namespace WPF_canvas
                 double dX2 = (r + 10) * Math.Cos(i * 30 / 180.0 * Math.PI);
                 */
 
-                double dY1 = 7 / 8.0 * r * Math.Sin(i * 30 / 180.0 * Math.PI);
-                double dX1 = 7 / 8.0 * r * Math.Cos(i * 30 / 180.0 * Math.PI);
-                double dY2 = 9 / 8.0 * r * Math.Sin(i * 30 / 180.0 * Math.PI);
-                double dX2 = 9 / 8.0 * r * Math.Cos(i * 30 / 180.0 * Math.PI);
+                double kisR1 = 7 / 8.0 * r;
+                double kisR2 = 9 / 8.0 * r;
+
+                double dY1 = kisR1 * Math.Sin(i * 30 / 180.0 * Math.PI);
+                double dX1 = kisR1 * Math.Cos(i * 30 / 180.0 * Math.PI);
+                double dY2 = kisR2 * Math.Sin(i * 30 / 180.0 * Math.PI);
+                double dX2 = kisR2 * Math.Cos(i * 30 / 180.0 * Math.PI);
 
                 vonal.X2 = x + r + dX;
                 vonal.Y2 = y + r + dY;
 
                 vonal.X1 = x + r + dX1;
                 vonal.Y1 = y + r + dY1;
+
                 vonal.X2 = x + r + dX2;
                 vonal.Y2 = y + r + dY2;
 
@@ -105,15 +147,15 @@ namespace WPF_canvas
         {
             canvas.Children.Clear();
             kor(100, 100, 100, szogAllas * 10);
-            
+
             szogAllas++;
-            
-            if(szogAllas >= 360)
+
+            if (szogAllas >= 360)
             {
                 szogAllas -= 360;
                 timer.Stop();
             }
-            
+
         }
 
         void kor(int x, int y, int sugar, int szog)
