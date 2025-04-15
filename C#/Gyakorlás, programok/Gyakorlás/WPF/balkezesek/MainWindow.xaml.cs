dd2d5e6ff3a337eb25a674202f909567676a40e0
RTomi05 <rengel.tamas.22@ady-nagyatad.hu>
2025-04-15 11:25:24 +0200

    mókus

---
+++ C#/Gyakorlás, programok/Gyakorlás/WPF/balkezesek/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace balkezesek
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        List<Jatekos> jatekosok = new List<Jatekos>();
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            string[] sorok = File.ReadAllLines("balkezesek.csv");
+
+            for (int i = 1; i < sorok.Length; i++)
+            {
+                jatekosok.Add(Jatekos.Factory(sorok[i]));
+            }
+        }
+    }
+
+}
---
+++ C#/Gyakorlás, programok/Gyakorlás/WPF/balkezesek/jatekos.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace balkezesek
+{
+    internal class Jatekos
+    {
+        public string nev;
+        public DateOnly elsodatum;
+        public DateOnly utolsoDatum;
+        public int suly;
+        public int magassag;
+
+        public Jatekos(string nev, DateOnly elsoDatum, DateOnly utolsoDatum, int suly, int magassag)
+        {
+            this.nev = nev;
+            this.elsodatum = elsoDatum;
+            this.utolsoDatum = utolsoDatum;
+            this.suly = suly;
+            this.magassag = magassag;
+        }
+        public static Jatekos Factory(string sor)
+        {
+            //"Jim Abbott;1989-04-08;1999-07-21;200;75"
+            string[] vag = sor.Split(';');
+            return new Jatekos(vag[0], DateOnly.Parse(vag[1]), DateOnly.Parse(vag[2]), int.Parse(vag[3]), int.Parse(vag[4]));
+        }
+    }
+}
