c239427840c202bfa5c5d26361a1366bfa821111
RTomi05 <rengel.tamas.22@ady-nagyatad.hu>
2025-05-28 14:04:25 +0200

    na

---
+++ C#/Gyakorlás, programok/Gyakorlás/WPF/utca_WPF/KeritesSzin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace utca_WPF
+{
+    class KeritesSzin
+    {
+        public string kod;
+        public string nev;
+        public SolidColorBrush szin;
+
+        public KeritesSzin(string kod, string nev, SolidColorBrush szin)
+        {
+            this.kod = kod;
+            this.nev = nev;
+            this.szin = szin;
+        }
+    }
+}
---
+++ C#/Gyakorlás, programok/Gyakorlás/WPF/utca_WPF/Telek.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace utca_WPF
+{
+    class Telek
+    {
+        public bool paros;
+        public int szelesseg;
+        public KeritesSzin kerites;
+
+        public Telek(bool paros, int szelesseg, KeritesSzin kerites)
+        {
+            this.paros = paros;
+            this.szelesseg = szelesseg;
+            this.kerites = kerites;
+        }
+    }
+}
