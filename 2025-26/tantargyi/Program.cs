53b7e5541579f69ccb4af2f86e3abd5a8ffc28bf
unknown <rengel.tamas.22@ady-nagyatad.lan>
2025-09-03 11:25:23 +0200

    nem

---
+++ 2025-26/tantargyi/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace tantargyi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //egész szám bekérése, listába készítsünk annyi 1 - 5 véletlenszámot -> átlag
+            Random rand = new Random();
+            List<int> szamok = new List<int>();
+            string tantargy = "alma";
+            int szam = 0;
+            while (tantargy != "")
+            {
+                Console.Write("Kérem a tantárgy nevét: ");
+                tantargy = Console.ReadLine();
+                if (tantargy == "")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.Write("Kérem az érdemjegyek számát: ");
+                    szam = int.Parse(Console.ReadLine());
+                    for (int i = 0; i < szam; i++)
+                    {
+                        int veletlenSzam = rand.Next(1, 6);
+                        szamok.Add(veletlenSzam);
+                    }
+                }
+            }
+            Console.WriteLine("Számok egy sorban: " + string.Join(", ", szamok));
+            double atlag = szamok.Average();
+            Console.WriteLine("Az érdemjegyek átlaga: " + atlag);
+
+        }
+    }
+}
