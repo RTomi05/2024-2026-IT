20c10b92ebacffa762d36788538552ab3616f46c
unknown <rengel.tamas.22@ady-nagyatad.lan>
2025-09-03 09:40:48 +0200

    elso25-26

---
+++ 2025-26/elsoIsmetles/Program.cs
@@ -0,0 +1,46 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //bekérünk egy számot a felhasználótól, és az első 10 többszörösét kiírjuk
+        Console.Write("Kérek egy számot: ");
+        int szam = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i <= 10; i++)
+        {
+            Console.WriteLine(szam * i);
+        }
+        Console.WriteLine();
+
+
+
+        Console.Write("Kérem a piramis magasságát: ");
+        int probaMagassag = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= probaMagassag; i++)
+        {
+            for (int j = probaMagassag; j <= i; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+
+
+
+        //piramis építése
+        Console.Write("Kérem a piramis magasságát: ");
+        int magassag = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= magassag; i++)
+        {
+            for (int j = magassag; j > i; j--)
+            {
+                Console.Write(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++)
+            {
+                Console.Write("#");
+            }
+            Console.WriteLine();
+        }
+    }
+}
