9eb9f48d4f73c107adca7ff663b9bc7e973c3a31
unknown <rengel.tamas.22@ady-nagyatad.lan>
2025-09-18 11:24:51 +0200

    oop

---
+++ 2025-26/Asztali/oop_gyak/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace oop_gyak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Konyv k = new Konyv("Bűnben születtem", "Trevor Noah");
+
+            Console.WriteLine(k);
+        }
+    }
+}
