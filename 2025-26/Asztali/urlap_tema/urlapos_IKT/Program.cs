3da83a91471b97626808ff129ee68e881505b6d2
unknown <rengel.tamas.22@ady-nagyatad.lan>
2025-10-13 08:41:43 +0200

    nem kéne

---
+++ 2025-26/Asztali/urlap_tema/urlapos_IKT/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace urlapos_IKT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //űrlap, fájlból betöltés / fájlba mentés, legalább 8 adat bevitele, lista módosítása, esetleg szűrés,
+            //deadline: november vége
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
