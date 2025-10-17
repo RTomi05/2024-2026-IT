d9372fea2eaee3f7797e3e70c228a7dad8abca75
RTomi05 <120109540+RTomi05@users.noreply.github.com>
2025-10-17 09:57:25 +0200

    Add files via upload

---
+++ 2025-26/Asztali/gyakorlasOsszes/Program.cs
@@ -0,0 +1,16 @@
+﻿using OOPgyak6;
+
+namespace majdElnevezem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            KapucnisPulover kp = new KapucnisPulover("Pamut");
+            kp.Meret = "L";
+            Console.WriteLine(kp);
+            kp.mosas();
+            //ruha interface anyag tulajdonsággal, ami csak lekérdezhető, abstract class Pulover néven konstruktorral, osztaly kapucnis pulóver néven
+        }
+    }
+}
