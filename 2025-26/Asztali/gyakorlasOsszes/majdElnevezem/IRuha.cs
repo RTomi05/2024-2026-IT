76d63d629c3c939074a3e2e64c8078865ae7d5fa
RTomi05 <rengel.tamas.22@ady-nagyatad.hu>
2025-10-17 09:56:14 +0200

    na

---
+++ 2025-26/Asztali/gyakorlasOsszes/majdElnevezem/IRuha.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace majdElnevezem
+{
+    internal interface IRuha
+    {
+        public string Meret { get; set; }
+        public string Anyag { get; }
+    }
+}
---
+++ 2025-26/Asztali/gyakorlasOsszes/majdElnevezem/KapucnisPulover.cs
@@ -0,0 +1,25 @@
+﻿using majdElnevezem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPgyak6
+{
+    internal class KapucnisPulover:Pulover
+    {
+        public KapucnisPulover(string anyag):base(anyag)
+        {
+
+        }
+        public override void mosas()
+        {
+            Console.WriteLine("A pulóver mosása 30 fokon ajánlott.");
+        }
+        public override string ToString()
+        {
+            return $"Kapucnis pulóver anyaga: {Anyag}, mérete: {Meret}";
+        }
+    }
+}
