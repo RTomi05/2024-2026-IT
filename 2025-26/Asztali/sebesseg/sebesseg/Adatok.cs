9f9603b53debad2378d2b1dc8685eaf24567980d
rengel.tamas.22 <rengel.tamas.22@ady-nagyatad.hu>
2025-11-04 11:25:33 +0100

    forgalmi

---
+++ 2025-26/Asztali/sebesseg/sebesseg/Adatok.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sebesseg
+{
+    internal class Adatok
+    {
+        public int km;
+        public string jelzes;
+        public bool varosban;
+
+        public Adatok(string sor)
+        {
+            string[] vag = sor.Split(' ');
+            km = int.Parse(vag[0]);
+            jelzes = vag[1];
+        }
+
+        public Adatok(int km, string jelzes)
+        {
+            this.km = km;
+            this.jelzes = jelzes;
+        }
+
+        public bool isTelepules()
+        {
+            return jelzes.Length >= 4;
+        }
+
+        public int sebessegHatar()
+        {
+            try
+            {
+                int sebesseg = int.Parse(jelzes);
+                return sebesseg;
+            }
+            catch (Exception)
+            {
+                if(isTelepules())
+                {
+                    return 50;
+                }
+                else if(jelzes == "]")
+                {
+                    return 90;
+                }
+                else if (jelzes == "#")
+                {
+                    if (varosban)
+                    {
+                        return 50;
+                    }
+                    else
+                    {
+                        return 90;
+                    }
+                }
+                else if (jelzes == "%")
+                {
+                    if (varosban)
+                    {
+                        return 50;
+                    }
+                    else
+                    {
+                        return 90;
+                    }
+                }
+                else
+                {
+                    return 0;
+                }
+                throw;
+            }
+        }
+    }
+}
---
+++ 2025-26/Asztali/sebesseg/sebesseg/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace sebesseg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sorok = File.ReadAllLines("ut.txt");
+
+            List<Adatok> adatLista = new List<Adatok>();
+
+            int teljesUtHossz = int.Parse(sorok[0]);
+
+            bool varosban = false;
+
+            for (int i = 1; i < sorok.Length; i++)
+            {
+                adatLista.Add(new Adatok(sorok[i]));
+                adatLista[i-1].isTelepules();
+            }
+
+
+            Console.WriteLine("2.feladat");
+
+            for (int i = 0; i < adatLista.Count; i++)
+            {
+                if (adatLista[i].isTelepules())
+                {
+                    Console.WriteLine(adatLista[i].jelzes);
+                }
+            }
+
+        }
+    }
+}
