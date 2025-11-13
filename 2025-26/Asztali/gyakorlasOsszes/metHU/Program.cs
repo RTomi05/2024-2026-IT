b128d8a928027731bf20c8734c88e3cfb5c037ce
RTomi05 <rengel.tamas.22@ady-nagyatad.hu>
2025-11-13 13:10:06 +0100

    z

---
+++ 2025-26/Asztali/gyakorlasOsszes/metHU/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace metHU
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sorok = File.ReadAllLines("tavirathu13.txt");
+            List<Adatok> adatLista = new List<Adatok>();
+
+            adatLista = sorok.Select(sor => new Adatok(sor)).ToList();
+        }
+    }
+}
