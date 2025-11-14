d38ed02f58c19654fd537fdc4a4670032ddbcd54
unknown <rengel.tamas.22@ady-nagyatad.lan>
2025-11-14 10:54:30 +0100

    pot

---
+++ 2025-26/Asztali/gyakorlasOsszes/telepuleses/Adatok.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metHU
+{
+    internal class Adatok
+    {
+        //BP 0000 VRB02 23
+        public string telepules;
+        public string ido;
+        public string szeliranyEsErosseg;
+        public int homerseklet;
+
+        public Adatok(string sor)
+        {
+            string[] vag = sor.Split(' ');
+
+            eltarol(vag[0], vag[1], vag[2], int.Parse(vag[3]));
+        }
+
+        public Adatok(string telepules, string ido, string szeliranyEsErosseg, int homerseklet)
+        {
+            eltarol(telepules, ido, szeliranyEsErosseg, homerseklet);
+        }
+        void eltarol(string telepules, string ido, string szeliranyEsErosseg, int homerseklet)
+        {
+            this.telepules = telepules;
+            this.ido = ido;
+            this.szeliranyEsErosseg = szeliranyEsErosseg;
+            this.homerseklet = homerseklet;
+        }
+    }
+}
---
+++ 2025-26/Asztali/gyakorlasOsszes/telepuleses/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace metHU
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sorok = File.ReadAllLines("tavirathu13.txt");
+            List<Adatok> adatLista = new List<Adatok>();
+
+            adatLista = sorok.Select(sor => new Adatok(sor)).ToList();
+            Console.WriteLine("2. feladat");
+            Console.Write("Adja meg egy település kódját! Település: ");
+            string varosKod = Console.ReadLine().ToUpper();
+            var utolsoMeresAdat = adatLista
+                .Where(adat => adat.telepules == varosKod)
+                .Select(adat => adat.ido)
+                .OrderBy(ido => ido)
+                .Last();
+            Console.WriteLine("Az utolsó mérési adat a megadott településről 23:45-kor érkezett.");
+        }
+    }
+}
