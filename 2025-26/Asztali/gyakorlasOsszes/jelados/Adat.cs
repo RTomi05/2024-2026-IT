2aa8ab79c99114724731509d71a72e032cab83b3
rengel.tamas.22 <rengel.tamas.22@ady-nagyatad.hu>
2025-11-26 11:26:08 +0100

    Hadházi

---
+++ 2025-26/Asztali/gyakorlasOsszes/jelados/Adat.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jelados
+{
+    internal class Adat
+    {
+        public int ora;
+        public int perc;
+        public int masodperc;
+        public int x;
+        public int y;
+        public Adat(int ora, int perc, int masodperc, int x, int y)
+        {
+            eltarol(ora, perc, masodperc, x, y);
+        }
+        public Adat(string sor)
+        {
+            string[] vag = sor.Split(" ");
+            eltarol(Convert.ToInt32(vag[0]),
+                    Convert.ToInt32(vag[1]),
+                    Convert.ToInt32(vag[2]),
+                    Convert.ToInt32(vag[3]),
+                    Convert.ToInt32(vag[4]));
+        }
+
+        void eltarol(int ora, int perc, int masodperc, int x, int y)
+        {
+            this.ora = ora;
+            this.perc = perc;
+            this.masodperc = masodperc;
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
---
+++ 2025-26/Asztali/gyakorlasOsszes/jelados/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace jelados
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string[] sorok = File.ReadAllLines("jel.txt");
+        List<Adat> adatok = new List<Adat>();
+        
+        sorok
+            .ToList()
+            .ForEach(x => {
+                adatok.Add(new Adat(x));
+            });
+        adatok = sorok.ToList().Select(x => new Adat(x)).ToList();
+    }
+}
