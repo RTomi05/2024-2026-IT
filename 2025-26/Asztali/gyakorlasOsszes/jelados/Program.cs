97110be127ea54ac99caa0ab9beb802c2c2ba188
RTomi05 <rengel.tamas.22@ady-nagyatad.hu>
2025-12-05 09:40:29 +0100

    pet

--- 2025-26/Asztali/gyakorlasOsszes/jelados/Program.cs
+++ 2025-26/Asztali/gyakorlasOsszes/jelados/Program.cs
@@ -20,12 +20,39 @@
             Console.WriteLine("2. feladat");
             Console.Write("Adja meg a jel sorszámát! ");
             int beSzam = int.Parse(Console.ReadLine());
+
+            var megfeleloJel = adatok[beSzam - 1];
+
+            Console.WriteLine($"x={megfeleloJel.x} y={megfeleloJel.y}");
             Console.WriteLine(adatok[beSzam - 1].koordinatak());
             //Console.WriteLine(adatok.Where((e,i) => i == beSzam - 1).First().koordinatak());
 
             Console.WriteLine();
             Console.WriteLine("4. feladat\nIdőtartam: {0}",
                                            adatok[0].elteltMasodperc(adatok.Last()));
+
+            Console.WriteLine();
+
+            Console.WriteLine("5.feladat");
+
+            int minX = adatok.Min(x => x.x);
+
+            int maxX = adatok.Max(x => x.x);
+
+            int minY = adatok.Min(y => y.y);
+
+            int maxY = adatok.Max(y => y.y);
+
+
+            int[] tomb = { adatok.Min(x => x.x), adatok.Max(x => x.x), adatok.Min(y => y.y), adatok.Max(y => y.y) };
+
+            var teglalap = new
+            {
+                balalso = new { x = adatok.Min(x => x.x), y = adatok.Min(y => y.y) },
+                jobbFelso = new { x = adatok.Max(x => x.x), y = adatok.Max(y => y.y) }
+            };
+
+            Console.WriteLine($"Bal alsó: {teglalap.balalso.x} {teglalap.balalso.y}, jobb felső: {teglalap.jobbFelso.x} {teglalap.jobbFelso.y}");
 
             Console.WriteLine();
             Console.WriteLine("6. feladat");
