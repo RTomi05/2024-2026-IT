6b459e207c914f1ea344f7ec587d3b1767a3267e
rengel.tamas.22 <rengel.tamas.22@ady-nagyatad.hu>
2025-12-08 10:01:55 +0100

    eeeej

--- 2025-26/Asztali/gyakorlasOsszes/jelados/Program.cs
+++ 2025-26/Asztali/gyakorlasOsszes/jelados/Program.cs
@@ -30,7 +30,10 @@
             Console.WriteLine();
             Console.WriteLine("6. feladat");
             var osszeg = adatok.Skip(1).Select((x, index) => x.tavolsag(adatok[index])).Sum();
-            Console.WriteLine($"Elmozdulás {osszeg:0.000} egység"); 
+            Console.WriteLine($"Elmozdulás {osszeg:0.000} egység");
+
+            Console.WriteLine();
+            Console.WriteLine("7. feladat");
 
         }
     }
