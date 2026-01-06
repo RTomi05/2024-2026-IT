42e94d1071c25d94ab5c332b9db1bbbb56c3eb74
RTomi05 <rengel.tamas.22@ady-nagyatad.hu>
2026-01-06 11:25:18 +0100

    statix fgv

---
+++ 2025-26/Asztali/oopStatic/Haromszoges.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopStatic
+{
+    internal class Haromszoges
+    {
+        public static double c(double a, double b)
+        {
+            return Math.Sqrt(a * a + b * b);
+        }
+
+        public static double Kerulet(double a, double b, double c)
+        {
+            return a + b + c;
+        }
+    }
+}
---
+++ 2025-26/Asztali/oopStatic/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace oopStatic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //példányosítás nélkül is elérhető a statikus osztály
+
+            Proba p1 = new Proba();
+            p1.fv2();
+
+            Proba.fv1();
+            Console.WriteLine(Proba.egyik);
+            Proba.egyik = "Másik";
+            Console.WriteLine(Proba.egyik);
+
+            //Új osztály, aminek van egy static függvénye -> a és b paraméter segítségével c oldalt számol
+            ////majd visszaadja számként
+            ///Másik függvény, ami 3 oldalt kap és visszaadja a kerületet
+            ///Static függvény, ami kiszámolja a háromszög területét (3 oldalt kap meg)
+            ///
+            Console.WriteLine(Haromszoges.c(3, 4));
+            Console.WriteLine(Haromszoges.Kerulet(3,5,6));
+        }
+    }
+}
