db28eaae81ef8017430d274fe1a4ebbefb57c532
RTomi05 <rengel.tamas.22@ady-nagyatad.hu>
2026-01-14 11:56:51 +0100

    jaj

---
+++ 2025-26/Asztali/gyakorlasOsszes/sipalya/Palya.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sipalya
+{
+    internal class Palya
+    {
+        public int ho;
+        public Palya(int ho)
+        {
+            this.ho = ho;
+        }
+
+        public static Palya operator +(Palya p1, Palya p2)
+        {
+            return new Palya(p1.ho + p2.ho);
+        }
+
+        public static Palya operator +(Palya p1, int vastagsag)
+        {
+            return new Palya(p1.ho + vastagsag);
+        }
+
+        public static int operator +(int vastagsag, Palya p1)
+        {
+            return p1.ho + vastagsag;
+        }
+        public static Palya operator -(Palya p1, Palya p2)
+        {
+            return new Palya(p1.ho - p2.ho);
+        }
+
+        public static Palya operator -(Palya p1, int p2)
+        {
+            return new Palya(p1.ho - p2);
+        }
+
+        public static Palya operator -(int p1, Palya p2)
+        {
+            return new Palya(p1 - p2.ho);
+        }
+
+        public static bool operator <(Palya p1, Palya p2)
+        {
+            return p1.ho < p2.ho;
+        }
+
+        public static bool operator >(Palya p1, Palya p2)
+        {
+            return p1.ho > p2.ho;
+        }
+
+        public static bool operator <=(Palya p1, Palya p2)
+        {
+            return p1.ho <= p2.ho;
+        }
+
+        public static bool operator >=(Palya p1, Palya p2)
+        {
+            return p1.ho >= p2.ho;
+        }
+
+        public static bool operator ==(Palya p1, Palya p2)
+        {
+            return p1.ho == p2.ho;
+        }
+
+        public static bool operator !=(Palya p1, Palya p2)
+        {
+            return p1.ho != p2.ho;
+        }
+
+        public static Palya operator *(Palya p1, int p2)
+        {
+            return new Palya(p1.ho * p2);
+        }
+    }
+}
---
+++ 2025-26/Asztali/gyakorlasOsszes/sipalya/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace sipalya
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Palya p1 = new Palya(20);
+            Palya p2 = new Palya(20);
+            Palya p3 = new Palya(20);
+
+            Console.WriteLine((p1 + p2).ho);
+            var p4 = p1 + p2;
+            Console.WriteLine(p4.ho);
+
+            Console.WriteLine(p1 == p2);
+            Console.WriteLine(p1 <= p2);
+            Console.WriteLine(p1 + p2 <= p2 + p3);
+            var p5 = p1 + 100;
+            p1 += 100;
+            Console.WriteLine(p1.ho);
+
+            p1 *= 2;
+        }
+    }
+}
