f5ca1e3658791d0abe981c3a0a744d09ffe7ad09
rengel.tamas.22 <rengel.tamas.22@ady-nagyatad.hu>
2026-01-20 11:26:55 +0100

    na

---
+++ 2025-26/Asztali/jurgen/Buksza.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jurgen
+{
+    internal class Buksza
+    {
+        //pénztárca -> objektum
+        //készpénz -> szám
+
+        public int penz;
+        public Buksza(int penz)
+        {
+            this.penz = penz;
+        }
+
+        public static Buksza operator -(Buksza buksza, int kPenz)
+        {
+            return new Buksza(buksza.penz - kPenz);
+        }
+
+        public static Buksza operator +(Buksza buksza, int kPenz)
+        {
+            return new Buksza(buksza.penz + kPenz);
+        }
+
+        public static Buksza operator -(Buksza buksza, Buksza buksza2)
+        {
+            return new Buksza(buksza.penz - buksza2.penz);
+        }
+
+        public static Buksza operator +(Buksza buksza, Buksza buksza2)
+        {
+            return new Buksza(buksza.penz + buksza2.penz);
+        }
+
+        public static bool operator >(Buksza Jurgenbuksza, Buksza buksza2)
+        {
+            return Jurgenbuksza.penz > buksza2.penz;
+        }
+
+        public static bool operator <(Buksza Jurgenbuksza, Buksza buksza2)
+        {
+            return Jurgenbuksza.penz < buksza2.penz;
+        }
+
+
+
+        public override string ToString()
+        {
+            return $"A pénztárcában {this.penz} euró van.";
+        }
+    }
+
+    
+}
---
+++ 2025-26/Asztali/jurgen/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace jurgen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Buksza bukszaJurgen = new Buksza(100);
+            Console.WriteLine(bukszaJurgen);
+            bukszaJurgen = bukszaJurgen - 30;
+            Console.WriteLine($"A hansi kért pénzt. {bukszaJurgen}");
+            bukszaJurgen = bukszaJurgen - 50;
+            Console.WriteLine(bukszaJurgen);
+            Buksza bukszaMaria = new Buksza(700);
+            Console.WriteLine(bukszaMaria);
+
+            if (bukszaJurgen < bukszaMaria)
+            {
+                Console.WriteLine("HOL A PÉNZ?!");
+            }
+            else
+            {
+                bukszaJurgen = bukszaJurgen - bukszaMaria;
+                Console.WriteLine(bukszaJurgen);
+                Console.WriteLine("JÓL ÁLL A SORAINKNAK!");
+            }
+        }
+    }
+}
