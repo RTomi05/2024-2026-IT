65db9c9de08e04b6a382211dae8f921b1ee68b06
rengel.tamas.22 <rengel.tamas.22@ady-nagyatad.hu>
2026-01-21 11:25:48 +0100

    GünterJürgen

---
+++ 2025-26/Asztali/jurgenMukodo/Penztarca.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jurgenMukodo
+{
+    internal class Penztarca
+    {
+        public int osszeg;
+        public Penztarca(int osszeg)
+        {
+            this.osszeg = osszeg;
+        }
+
+        public static Penztarca operator-(Penztarca p1, int osszeg)
+        {
+            return new Penztarca(p1.osszeg - osszeg);
+        }
+
+        public static Penztarca operator -(Penztarca p1, Penztarca p2)
+        {
+            return new Penztarca(p1.osszeg - p2.osszeg);
+        }
+
+        public static int operator -(int osszeg, Penztarca p1)
+        {
+            return osszeg - p1.osszeg;
+        }
+
+        public static Penztarca operator +(Penztarca p1, int osszeg)
+        {
+            return new Penztarca(p1.osszeg + osszeg);
+        }
+
+        public static Penztarca operator +(Penztarca p1, Penztarca p2)
+        {
+            return new Penztarca(p1.osszeg + p2.osszeg);
+        }
+
+        public static int operator +(int osszeg, Penztarca p1)
+        {
+            return osszeg + p1.osszeg;
+        }
+
+        public static bool operator <(Penztarca p1, Penztarca p2)
+        {
+            return p1.osszeg < p2.osszeg;
+        }
+
+        public static bool operator >(Penztarca p1, Penztarca p2)
+        {
+            return p1.osszeg > p2.osszeg;
+        }
+
+        public override string ToString()
+        {
+            return $"A pénztárcában {osszeg} Euro van.";
+        }
+    }
+}
---
+++ 2025-26/Asztali/jurgenMukodo/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace jurgenMukodo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Penztarca jurgen = new Penztarca(1000);
+            Console.WriteLine(jurgen);
+            int zsebpenz = 200;
+            jurgen = jurgen-zsebpenz;
+            Console.WriteLine(jurgen);
+
+            Penztarca maria = new Penztarca(1000);
+            if (jurgen > maria)
+            {
+                jurgen -= maria;
+                maria -= maria;
+            }
+            else
+            {
+                Console.WriteLine("ELVESZTETTE KÖZPÉNZ JELLEGÉT!");
+            }
+            Console.WriteLine(jurgen);
+
+            jurgen += 500;
+            Console.WriteLine(jurgen);
+            jurgen += new Penztarca(1500);
+            Console.WriteLine(jurgen);
+            jurgen += 500;
+            Console.WriteLine(jurgen);
+            Console.WriteLine(maria);
+
+            int eddigiAjandek = 23610;
+            Penztarca jurgenAdomany = new Penztarca(1000);
+            eddigiAjandek += jurgenAdomany;
+            jurgen -= jurgenAdomany;
+            Console.WriteLine(jurgen);
+            Console.WriteLine(eddigiAjandek);
+
+            int Gunter = 12000;
+            jurgen -= Gunter;
+            Console.WriteLine(jurgen);
+        }
+    }
+}
