2aaa26ad1059314957f66dcc5b9b7c93481ca8a1
RTomi05 <rengel.tamas.22@ady-nagyatad.hu>
2026-01-22 13:16:02 +0100

    nem

---
+++ 2025-26/Asztali/gyakorlasOsszes/gyakorloRegEx/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+namespace gyakorloRegEx
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string szoveg = "Lorem ipsum dolor sit amet," +
+               "consectetur adipiscing elit." +
+               "Ut dapibus orci id nulla accumsan lacinia." +
+               "Phasellus suscipit eros quis malesuada porttitor." +
+               "Sed porta nunc nec velit aliquet tempor.";
+            Regex regex = new Regex(@".{2}met");
+            var eredmeny = regex.Matches(szoveg);
+            Console.WriteLine(eredmeny[0].Value);
+            Console.WriteLine(eredmeny.Count);
+
+            //hány négybetűs szó van a szövegben
+            regex = new Regex(@"\w{4}");
+            eredmeny = regex.Matches(szoveg);
+            Console.WriteLine(eredmeny[0].Value);
+            Console.WriteLine(eredmeny.Count);
+
+        }
+    }
+}
---
+++ 2025-26/Asztali/valamiMajdAtnevezem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace valamiMajdAtnevezem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
