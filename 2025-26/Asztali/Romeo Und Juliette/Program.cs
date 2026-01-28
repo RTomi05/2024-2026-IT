b1cb865ced26f8b2e936481682ca66c0f8cc254c
RTomi05 <rengel.tamas.22@ady-nagyatad.hu>
2026-01-28 11:25:36 +0100

    huh

---
+++ 2025-26/Asztali/Romeo Und Juliette/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Romeo_Und_Juliette
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string forras = File.ReadAllText("RomeoAndJuliette.txt");
+
+            Regex rg = new Regex(@"[A-ZÖÜÓŐÚŰÉÁÍ-]{2,} [A-ZÖÜÓŐÚŰÉÁÍ-]{2,}|[A-ZÖÜÓŐÚŰÉÁÍ-]{2,}\b");
+            var eredmeny = rg.Matches(forras);
+
+            //Console.WriteLine(eredmeny.Count);
+
+            foreach (Match match in eredmeny)
+            {
+                Console.WriteLine(match.Value);
+            }
+        }
+    }
+}
