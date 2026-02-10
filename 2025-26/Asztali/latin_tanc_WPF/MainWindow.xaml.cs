c905ba18b8907f149bd1d630ec7cab8b0ff748a5
rengel.tamas.22 <rengel.tamas.22@ady-nagyatad.hu>
2026-02-10 11:25:29 +0100

    nono

---
+++ 2025-26/Asztali/latin_tanc_WPF/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace latin_tanc_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //1. feladat
+            var adatok = File.ReadAllLines("tancrend.txt");
+            List<Tanc> tancok = new List<Tanc>();
+            var adatok2Chunk = adatok.Chunk(3).ToList();
+
+            foreach (var adat in adatok2Chunk)
+            {
+                tancok.Add(new Tanc(adat.ToList()));
+            }
+            elsoTancKiir.Content = tancok[0].tancNev;
+            utolsoTancKiir.Content = tancok[tancok.Count - 1].tancNev;
+
+            int sambaSzamlalo = 0;
+            sambaSzamlalo = tancok.Where(x => x.tancNev == "samba").Count();
+        }
+
+        private void legtobbTanc_Click(object sender, RoutedEventArgs e)
+        {
+            var psi = new System.Diagnostics.ProcessStartInfo("https://www.youtube.com/watch?v=9xp1XWmJ_Wo")
+            {
+                UseShellExecute = true
+            };
+            System.Diagnostics.Process.Start(psi);
+        }
+    }
+}
---
+++ 2025-26/Asztali/latin_tanc_WPF/Tanc.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace latin_tanc_WPF
+{
+    internal class Tanc
+    {
+        public string tancNev;
+        public string lany;
+        public string fiu;
+
+        public Tanc(List<string>adat)
+        {
+            tancNev = adat[0];
+            lany = adat[1];
+            fiu = adat[2];
+        }
+    }
+}
