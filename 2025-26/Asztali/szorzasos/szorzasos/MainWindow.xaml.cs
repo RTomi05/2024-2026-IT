56ab604a1c20d4521ecf08c79342d3dfda78fa1e
RTomi05 <rengel.tamas.22@ady-nagyatad.hu>
2026-02-13 10:31:18 +0100

    belepteto

---
+++ 2025-26/Asztali/szorzasos/szorzasos/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace szorzasos
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void kuldes_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void kep_GotFocus(object sender, RoutedEventArgs e)
+        { 
+        }
+
+        private void kep_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            int elsoSzam = int.Parse(elso.Text);
+            int masodikSzam = int.Parse(masodik.Text);
+            int harmadikSzam = int.Parse(harmadik.Text);
+            int szorzat = 0;
+            List<int> szamok = new List<int>() { elsoSzam, masodikSzam, harmadikSzam };
+
+            szamok.Sort();
+            szorzat = szamok[0] * szamok[1];
+            kiiras.Content = $"A két legkisebb szám szorzata: {szorzat}";
+        }
+
+        private void kep_IsMouseDirectlyOverChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+        }
+    }
+}
