99e8f5b5bca43a7abaab479d80520674491ef396
unknown <rengel.tamas.22@ady-nagyatad.lan>
2026-03-04 09:40:43 +0100

    menetrendek

--- 2025-26/Webprogramozás/Gyakorlások/Menetrend/script.js
+++ 2025-26/Webprogramozás/Gyakorlások/Menetrend/script.js
@@ -6,20 +6,23 @@ function betolt()
     fetch(filenev)
     .then(x => x.text())
     .then(x => {
-        /*
-        console.log(x);
-        // 1 0   5   45
-        //let minta = /(\d+)\t(\d+)\t(\d+)\t([IE])/g;
-        let minta = /(\d+)\s+(\d+)\s+(\d+)\s+([IE])/g;
-        let eredmeny = x.match(minta);
-        console.log(eredmeny.groups, eredmeny);
-        */
        const regex = /^(?<vonatId>\d+)\s(?<allomasId>\d+)\s(?<ora>\d+)\s(?<perc>\d+)\s+(?<tipus>[IE])$/gm;
+
        let match;
+       
        while ((match = regex.exec(x)) !== null)
         {
             adatok.push(match.groups);
         }
         console.log(adatok);
     });
+}
+
+/*
+Írja a képernyőre a fájlban tárolt vonatok és állomások darabszámát – a kezdő és
+végállomást is beleértve! */
+
+function feladat2()
+{
+    console.log("2. feladat:");
 }
