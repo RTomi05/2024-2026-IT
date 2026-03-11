3f84f4c93ef2a052864dfe1129b9eba962d60804
rengel.tamas.22 <rengel.tamas.22@ady-nagyatad.hu>
2026-03-11 11:26:34 +0100

    1200 fájl

---
+++ 2025-26/Asztali/1200fajl/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _1200fajl
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1200 fájl létrehozása
+            for (int i = 1; i <= 1200; i++)
+            {
+                string fileName = $"a_{i}.txt";
+                File.WriteAllText(fileName, "a");
+            }
+        }
+    }
+}
