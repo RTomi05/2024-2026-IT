64104d6da86294fec177cc06539edb14fc900f08
rengel.tamas.22 <rengel.tamas.22@ady-nagyatad.hu>
2026-04-14 11:23:32 +0200

    szallitoSzalag

--- 2025-26/Asztali/szalag/szalag/MainWindow.xaml.cs
+++ 2025-26/Asztali/szalag/szalag/MainWindow.xaml.cs
@@ -20,12 +20,40 @@ namespace szalag
         public MainWindow()
         {
             InitializeComponent();
-
+            betoltes();
         }
 
+        List<Adat> adatok = new List<Adat>();
+        int szalagHossz = 0;
+        int sebesseg = 0;
         public void betoltes()
         {
             string[] sorok = File.ReadAllLines("szallit.txt");
+
+            string elsoSor = sorok[0];
+            szalagHossz = int.Parse(elsoSor.Split(" ")[0]);
+            sebesseg = int.Parse(elsoSor.Split(" ")[1]);
+            
+            foreach (string sor in sorok.Skip(1))
+            {
+                adatok.Add(new Adat(sor));
+            }
+            //MessageBox.Show($"Adatok száma: {adatok.Count}");
+        }
+
+        private void feladat2gomb_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int sorszam  = int.Parse(Sorszam.Text);
+                feladat2kiir.Foreground = Brushes.Black;
+                feladat2kiir.Content = $"Honnan: {adatok[sorszam-1].honnan}\nHova: {adatok[sorszam-1].hova}";
+            }
+            catch(Exception ex)
+            {
+                feladat2kiir.Foreground = Brushes.Red;
+                feladat2kiir.Content = "Hibás adat!";
+            }
         }
     }
 }
