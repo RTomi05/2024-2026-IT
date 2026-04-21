ce265e50168d5692fe0a9970e982259852da90bc
rengel.tamas.22 <rengel.tamas.22@ady-nagyatad.hu>
2026-04-21 11:25:41 +0200

    vizibicikli

---
+++ 2025-26/Asztali/vizibicikli/Kolcsonzes.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vizibicikli
+{
+    internal class Kolcsonzes
+    {
+        public string Nev;
+        public string Jarmu;
+        public int OraKi;
+        public int PercKi;
+        public int OraBe;
+        public int PercBe;
+
+        public Kolcsonzes(string sor)
+        {
+            string[] sorok = sor.Split(";");
+            Nev = sorok[0];
+            Jarmu = sorok[1];
+            OraKi = int.Parse(sorok[2]);
+            PercKi = int.Parse(sorok[3]);
+            OraBe = int.Parse(sorok[4]);
+            PercBe = int.Parse(sorok[5]);
+        }
+
+        public Kolcsonzes(string nev, string jarmu, int oraki, int percki, int orabe, int percbe)
+        {
+            Nev = nev;
+            Jarmu = jarmu;
+            OraKi = oraki;
+            PercKi = percki;
+            OraBe = orabe;
+            PercBe = percbe;
+        }
+
+        public string idoKi()
+        {
+            return $"{this.OraKi}:{this.PercKi:00}";
+        }
+        public string idoBe()
+        {
+            return $"{this.OraBe}:{this.PercBe:00}";
+        }
+
+        public string idoTartam()
+        {
+            return $"{idoKi()} - {idoBe()}";
+        }
+
+        public bool kintvan(string idopont)
+        {
+            //10:19
+
+            int percbenKi = this.OraKi * 60 + this.PercKi;
+
+            int percbenBe = this.OraBe * 60 + this.PercBe;
+
+            string[] vag = idopont.Split(":");
+
+            int percbenIdopont = int.Parse(vag[0]) * 60 + int.Parse(vag[1]);
+
+            return percbenKi <= percbenIdopont && percbenBe >= percbenIdopont;
+
+        }
+
+        public int Ar()
+        {
+            int percbenKi = this.OraKi * 60 + this.PercKi;
+
+            int percbenBe = this.OraBe * 60 + this.PercBe;
+
+            int idoTartam = percbenBe - percbenKi;
+
+            return (idoTartam / 30 + 1) * 2400;
+        }
+    }
+}
---
+++ 2025-26/Asztali/vizibicikli/MainWindow.xaml.cs
@@ -0,0 +1,277 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace vizibicikli
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            betolt();
+        }
+
+        List<Kolcsonzes> kolcsonzesek = new List<Kolcsonzes>();
+
+        public void betolt()
+        {
+            string[] sorok = File.ReadAllLines("kolcsonzesek.txt");
+
+            foreach (string s in sorok.Skip(1))
+            {
+                kolcsonzesek.Add(new Kolcsonzes(s));
+            }
+
+            //MessageBox.Show(kolcsonzesek.Count.ToString());
+            //9. feladat
+
+            var jarmuvek = kolcsonzesek.Select(x => x.Jarmu).Distinct().OrderBy(e => e).ToList();
+
+            Jarmuvalaszt.ItemsSource = jarmuvek;
+
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            f5Eredmeny.Content = kolcsonzesek.Count;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string nev = Nev.Text;
+
+            var szures = kolcsonzesek.Where(e => nev == e.Nev).Select(e => e.idoTartam()).ToList();
+
+
+
+            if (szures.Count > 0)
+            {
+                f6Hiba.Content = "";
+
+                f6eredmeny.ItemsSource = szures;
+            }
+            else
+            {
+                f6eredmeny.ItemsSource = "";
+                f6Hiba.Content = "Nincs ilyen nevű!";
+            }
+
+
+
+
+        }
+
+        private void keres_Click(object sender, RoutedEventArgs e)
+        {
+            string ido = Ido.Text;
+
+            try
+            {
+                var szures = kolcsonzesek.Where(e => e.kintvan(ido)).Select(x => $"{x.idoTartam()} : {x.Nev}").ToList();
+
+                f7kiir.ItemsSource = szures;
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+        }
+
+        private void Ido_KeyUp(object sender, KeyEventArgs e)
+        {
+
+            string ido = Ido.Text;
+
+            switch (ido.Length)
+            {
+                case 0:
+                    break;
+                case 1:
+                    try
+                    {
+                        int t = int.Parse(ido);
+                        if (t > 2)
+                        {
+                            throw new Exception();
+                        }
+                    }
+                    catch
+                    {
+                        Ido.Text = "";
+                    }
+                    break;
+
+                case 2:
+                    try
+                    {
+                        int t = int.Parse(ido);
+                        if (t > 23)
+                        {
+                            throw new Exception();
+                        }
+                        Ido.Text += ":";
+                    }
+                    catch
+                    {
+                        Ido.Text = ido.Substring(0, ido.Length - 1);
+                    }
+                    break;
+
+                case 3:
+                    try
+                    {
+                        int t = int.Parse(ido.Substring(0, 2));
+                        if (t > 23)
+                        {
+                            throw new Exception();
+                        }
+
+                        if (ido[2] != ':')
+                        {
+                            throw new Exception();
+                        }
+
+                    }
+                    catch
+                    {
+                        Ido.Text = ido.Substring(0, ido.Length - 1);
+                    }
+                    break;
+
+                case 4:
+                    try
+                    {
+                        int t = int.Parse(ido.Substring(0, 2));
+                        if (t > 23)
+                        {
+                            throw new Exception();
+                        }
+
+                        if (ido[2] != ':')
+                        {
+                            throw new Exception();
+                        }
+
+                        t = int.Parse(ido.Substring(3, 1));
+                        if (t > 5)
+                        {
+                            throw new Exception();
+                        }
+
+
+                    }
+                    catch
+                    {
+                        Ido.Text = ido.Substring(0, ido.Length - 1);
+                    }
+                    break;
+                case 5:
+                    try
+                    {
+                        int t = int.Parse(ido.Substring(0, 2));
+                        if (t > 23)
+                        {
+                            throw new Exception();
+                        }
+
+                        if (ido[2] != ':')
+                        {
+                            throw new Exception();
+                        }
+
+                        t = int.Parse(ido.Substring(3, 2));
+                        if (t > 59)
+                        {
+                            throw new Exception();
+                        }
+
+
+                    }
+                    catch
+                    {
+                        Ido.Text = ido.Substring(0, ido.Length - 1);
+                    }
+                    break;
+                default:
+
+                    Ido.Text = ido.Substring(0, 5);
+                    break;
+            }
+
+            Ido.CaretIndex = 500;
+
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            f8kiir.Content = kolcsonzesek.Sum(x => x.Ar());
+        }
+
+        private void Jarmuvalaszt_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox jarmuValaszt = sender as ComboBox;
+
+            var kivalasztott = jarmuValaszt.SelectedItem.ToString();
+
+            var kolcsonzesei = kolcsonzesek.Where(e => e.Jarmu == kivalasztott).Select(e => $"{e.idoTartam()} : {e.Nev}").ToList();
+
+            f9eredmeny.ItemsSource = kolcsonzesei;
+
+            File.WriteAllLines(jarmuValaszt.SelectedItem.ToString() + ".txt", kolcsonzesei);
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Dictionary<string,int> statisztika = new Dictionary<string, int>();
+            foreach (Kolcsonzes egyAdat in kolcsonzesek)
+            {
+                try
+                {
+                    statisztika[egyAdat.Jarmu]++;
+                }
+                catch
+                {
+                    statisztika[egyAdat.Jarmu] = 1;
+                }
+            }
+
+            f10eredmeny.Content = "";
+            /*
+            foreach(var item in statisztika)
+            {
+                //MessageBox.Show(item.ToString());
+                f10eredmeny.Content += $"{item.Key} : {item.Value}{Environment.NewLine}";
+            }
+            */
+            foreach (var item in statisztika.Keys.OrderBy(x => x))
+            {
+                //MessageBox.Show(item.ToString());
+                f10eredmeny.Content += $"{item} - {statisztika[item]}{Environment.NewLine}";
+            }
+
+            f10eredmeny.Content = string.Join(Environment.NewLine,
+                kolcsonzesek
+                .OrderBy(x => x.Jarmu)
+                .GroupBy(x => x.Jarmu)
+                .Select(x => $"{x.Key} - {x.Count()}")
+                .ToList()
+                );
+
+        }
+    }
+    }
