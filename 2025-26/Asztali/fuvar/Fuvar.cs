1c0a27a10a6d7afc673ad0f25691f85af6c2dc64
rengel.tamas.22 <rengel.tamas.22@ady-nagyatad.hu>
2026-04-22 09:39:33 +0200

    asztali

---
+++ 2025-26/Asztali/fuvar/Fuvar.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fuvar
+{
+    internal class Fuvar
+    {
+        public int azonosito;
+        public string indulas;
+        public int idotartam;
+        public double tavolsag;
+        public double viteldij;
+        public double borravalo;
+        public string fizetesimod;
+
+        public Fuvar(int azonosito, string indulas, int idotartam, double tavolsag, double viteldij, double borravalo, string fizetesimod)
+        {
+            this.azonosito = azonosito;
+            this.indulas = indulas;
+            this.idotartam = idotartam;
+            this.tavolsag = tavolsag;
+            this.viteldij = viteldij;
+            this.borravalo = borravalo;
+            this.fizetesimod = fizetesimod;
+        }
+
+        public Fuvar(string sor)
+        {
+            string[] vag = sor.Split(';');
+            this.azonosito = int.Parse(vag[0]);
+            this.indulas = vag[1];
+            this.idotartam = int.Parse(vag[2]);
+            this.tavolsag = double.Parse(vag[3]);
+            this.viteldij = double.Parse(vag[4]);
+            this.borravalo = double.Parse(vag[5]);
+            this.fizetesimod = vag[6];
+        }
+    }
+}
---
+++ 2025-26/Asztali/fuvar/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace fuvar
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            betolt();
+        }
+
+        List<Fuvar> fuvarok = new List<Fuvar>();
+        void betolt()
+        {
+            string[] sorok = File.ReadAllLines("fuvar.csv");
+            foreach (string sor in sorok.Skip(1))
+            {
+                Fuvar f = new Fuvar(sor);
+                fuvarok.Add(f);
+            }
+            //MessageBox.Show("Adatok betöltve!");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            eredmeny3.Content = fuvarok.Count();
+        }
+    }
+}
