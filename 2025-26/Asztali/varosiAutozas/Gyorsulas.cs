7fb8142cfb5435212493bb3cfab2418b585da351
RTomi05 <rengel.tamas.22@ady-nagyatad.hu>
2026-05-19 11:25:12 +0200

    varosAuto

---
+++ 2025-26/Asztali/varosiAutozas/Gyorsulas.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace varosiAutozas
+{
+    internal class Gyorsulas
+    {
+        public int kezdes;
+        public int veg;
+        public int sebesseg;
+        public Gyorsulas elozo;
+
+        public Gyorsulas(int kezdes, int veg, int sebesseg)
+        {
+            this.kezdes = kezdes;
+            this.veg = veg;
+            this.sebesseg = sebesseg;
+        }
+
+        public Gyorsulas(string sor)
+        {
+            string[] adatok = sor.Split("\t");
+            this.kezdes = int.Parse(adatok[0]);
+            this.veg = int.Parse(adatok[1]);
+            this.sebesseg = int.Parse(adatok[2]);
+        }
+
+        public Gyorsulas(string sor, Gyorsulas elozo)
+        {
+            string[] adatok = sor.Split("\t");
+            this.kezdes = int.Parse(adatok[0]);
+            this.veg = int.Parse(adatok[1]);
+            this.sebesseg = int.Parse(adatok[2]);
+            this.elozo = elozo;
+        }
+        public double gyorsulas()
+        {
+            return (sebesseg - elozo.sebesseg) / (veg - kezdes);
+        }
+
+        public double pillanatnyiSebesseg(int idopont)
+        {
+            if(idopont >= kezdes && idopont <= veg)
+            {
+                return elozo.sebesseg + (gyorsulas() * (idopont - kezdes));
+            }
+            else if(idopont > elozo.veg && idopont <= veg)
+            {
+                return elozo.sebesseg;
+            }    
+            return -1;
+        }
+    }
+}
---
+++ 2025-26/Asztali/varosiAutozas/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace varosiAutozas
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        List <Gyorsulas> gyorsulasok = new List<Gyorsulas>();
+
+        private void kuldes_Click(object sender, RoutedEventArgs e)
+        {
+            gyorsulasok.Clear();
+            string azonosito = azon.Text;
+            string[] sorok = File.ReadAllLines(azonosito+".txt");
+            foreach (string s in sorok)
+            {
+                gyorsulasok.Add(new Gyorsulas(s,gyorsulasok.LastOrDefault(new Gyorsulas(0,0,0))));
+            }
+        }
+
+        private void f2Lekeres_Click(object sender, RoutedEventArgs e)
+        {
+            indulas.Text = gyorsulasok.First().kezdes.ToString() + " s";
+            vegsebesseg.Text = gyorsulasok.Last().sebesseg.ToString() + " m/s";
+        }
+
+        private void f3Ellenorzes_Click(object sender, RoutedEventArgs e)
+        {
+            bool atlepte = gyorsulasok.Where(e => e.sebesseg > 14).Count() > 0;
+            atlepteE.Text = atlepte ? "Igen" : "Nem";
+        }
+
+        private void f4Kuldes_Click(object sender, RoutedEventArgs e)
+        {
+            var allas = new { kezdet = 0, veg = 0, tartam = 0};
+            for(int i  = 0; i < gyorsulasok.Count; i++)
+            {
+                if (gyorsulasok[i].sebesseg == 0)
+                {
+                    int tartam = gyorsulasok[i + 1].kezdes - gyorsulasok[i].veg;
+                    if(allas.tartam < tartam)
+                    {
+                        allas = new
+                        {
+                            kezdet = gyorsulasok[i].veg,
+                            veg = gyorsulasok[i + 1].kezdes,
+                            tartam = tartam
+                        };
+                    }
+                    
+                }
+            }
+            allasido.Text = $"{allas.kezdet} és {allas.veg} között volt.";
+        }
+
+        private void f5Szamitas_Click(object sender, RoutedEventArgs e)
+        {
+            int ido = int.Parse(idopont.Text);
+            var keresett = gyorsulasok.Where(e => (
+                (ido >= e.kezdes
+                && ido <= e.veg)
+                || (e.elozo.veg <= ido
+                && e.kezdes >= ido))).ToList();
+            if (keresett.Count > 0)
+            {
+                seb.Text = keresett.First().pillanatnyiSebesseg(ido).ToString() + " m/s";
+            }
+        }
+    }
+}
